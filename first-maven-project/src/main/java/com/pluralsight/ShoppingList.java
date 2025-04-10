cce2e2685316d65a40ffe3081454cb0a66244abc
Randyglobal <randyitoe2255@gmail.com>
2025-04-10 10:58:19 -0400

    Creating my first Java project

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+// your code goes here
+        System.out.println("Egg, Chicken, Bread, Milk, Butter, Biscuff, Orange, Banana,");
+    }
+}
