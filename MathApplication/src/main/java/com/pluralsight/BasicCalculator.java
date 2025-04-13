20a8b3189ec8e504c9e524e7924d9f60d1766f84
Randyglobal <randyitoe2255@gmail.com>
2025-04-13 13:28:45 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

--- MathApplication/src/main/java/com/pluralsight/BasicCalculator.java
+++ MathApplication/src/main/java/com/pluralsight/BasicCalculator.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 public class BasicCalculator {
     public static void main(String[] args) {
+//        Calling the Scanner
         Scanner scanner = new Scanner(System.in);
-
+//        Reading questions
         System.out.print("Enter the first number: ");
         int firstNumber = scanner.nextInt();
         System.out.println("Enter the second number: ");
@@ -13,10 +14,12 @@ public class BasicCalculator {
         System.out.println("Please Select an option:");
         System.out.println("Possible Calculations: \n (A)dd \n (S)ubtract \n (M)ultiply \n (D)ivide");
          String action = scanner.next().trim();
+//         passing condition
         int result;
         if(action.isEmpty()){
            System.out.print("Invalid Action");
         }else if(action.equalsIgnoreCase("a")){
+//            displaying results
             result = firstNumber * secondNumber;
             System.out.println("Result = " + result);
         }else if(action.equalsIgnoreCase("s")){
